59ed69065f83fcd3ca8d7270cd6850aee04e9778
Arjun Lal <arjun.l@synctric.in>
2025-12-18 16:48:19 +0530

    change in socket

--- src/server.ts
+++ src/server.ts
@@ -25,14 +25,3 @@ initSocket(io);
 httpServer.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
-
-// import app from "./app";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => {
-//     console.log(`Server running at http://localhost:${PORT}`);
-// });
