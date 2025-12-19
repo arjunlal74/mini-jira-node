98a0b1602daefac24e2fde2263dc5a45d6fe1aec
Arjun Lal <arjun.l@synctric.in>
2025-12-19 13:06:33 +0530

    changes

--- src/workers/misc.worker.ts
+++ src/workers/misc.worker.ts
@@ -1,8 +1,9 @@
 import { Worker } from 'bullmq';
 import { redisConnection } from '../queues/connection';
+import queueNames from '../enums/queueNames';
 
 new Worker(
-  'misc',
+  queueNames.MISC,
   async (job) => {
     const JobClass = require(`../jobs/${job.name}`).default;
     await JobClass.handle(job.data);
